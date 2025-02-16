aa083f0b9e4bbbd40abf9357a85b2400843566c0
Timo <timokhan2@gmail.com>
2025-02-16 21:47:21 +0100

    js committed

---
+++ agecal.js
@@ -0,0 +1,6 @@
+//Future years calculation
+const yearOfBirth = 1991;
+let yearFuture = 2027;
+let age = 2025-1991;
+yearFuture = 2027-1991;
+console.log("You will be " + yearFuture + " years old in 2027" + ".")
